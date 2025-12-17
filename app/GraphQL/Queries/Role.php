653c14bb459322a7599a72ee6f6cb98b0a83dd1f
kmahadeehasan-lgtm <kmahadeehasan@gmail.com>
2025-12-17 13:01:30 +0600

    get data by id

--- app/GraphQL/Queries/Role.php
+++ app/GraphQL/Queries/Role.php
@@ -2,11 +2,14 @@
 
 namespace App\GraphQL\Queries;
 
-final readonly class Test
+final readonly class Role
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
         return "GraphQL Works";
+    }
+    public function __show(){
+        return "Admin";
     }
 }
